0624caf7a1789fbc5436d2353c55524f0577e002
Hiddev666 <officehiddev@gmail.com>
2025-11-23 00:55:04 +0700

    test: auto deloy test

--- app/Views/pages/public/layouts/berita.php
+++ app/Views/pages/public/layouts/berita.php
@@ -8,7 +8,7 @@
             <p class="text-gray-500">Selengkapnya</p>
         </a>
     </div>
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-5" id="news-wrapper">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-5" id="news-wrapper">
         <!-- <div class="p-5 border border-gray-300 flex flex-col gap-1 rounded-md">
             <p class="text-gray-600">Selasa, 15 Juli 2025</p>
             <h2 class="text-lg font-bold text-[var(--primary)]">Membangun Generasi Cerdas Dengan Budaya Literasi</h2>
@@ -45,7 +45,7 @@
             const result = response;
 
             if (result.status === 'success' && Array.isArray(result.data) && result.data != '') {
-                filteredData = result.data.slice(0, 4)
+                filteredData = result.data.slice(0, 6)
                 filteredData.forEach(item => {
                     const card = document.createElement("div")
                     card.className = "p-5 border border-gray-300 flex flex-col gap-1 rounded-md"
@@ -55,25 +55,20 @@
                     date.innerHTML = formatRelativeTimeCustom(item.last_update)
 
                     const title = document.createElement("h2")
-                    title.className = "text-lg font-bold text-[var(--primary)]"
+                    title.className = "text-lg font-bold text-[var(--primary)] leading-5"
                     title.innerHTML = item.content_title
 
                     const body = document.createElement("p")
-                    body.className = "text-gray-800 line-clamp-2 text-justify leading-5"
+                    body.className = "text-gray-800 line-clamp-1 leading-5"
                     body.innerHTML = item.content_desc
 
                     const anchor = document.createElement("a")
                     anchor.href = `/news/${item.content_path}`
 
-                    const link = document.createElement("button")
-                    link.className = "text-[var(--primary)] text-start"
-                    link.innerHTML = "Selengkapnya"
-
-                    anchor.appendChild(link)
-                    card.appendChild(date)
+                    anchor.appendChild(card)
                     card.appendChild(title)
                     card.appendChild(body)
-                    card.appendChild(anchor)
+                    card.appendChild(date)
 
                     newsWrapper.appendChild(card)
                 });
